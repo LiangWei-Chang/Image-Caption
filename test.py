5db1bd57f7429785c54ff4cfff411f62c567a52b
LiangWei <lwchang9611@gmail.com>
2017-12-06 23:21:58 +0800

    add test.py

--- test.py
+++ test.py
@@ -2,13 +2,19 @@ import tensorflow as tf
 import _pickle as cPickle
 import pandas as pd
 import os
+import sys
+sys.path.append('model')
+sys.path.append('utils')
+from tqdm import tqdm
 
 def generate_captions(model, enc_map, dec_map, img_test, max_len=15):
     img_ids, caps = [], []
+    pbar = tqdm(total=len(img_test.items()))
     for img_id, img in img_test.items():
         img_ids.append(img_id)
         img = np.expand_dims(img, axis=0)
         caps.append(model.inference(img, enc_map, dec_map))
+        pbar.update(1)
     return pd.DataFrame({'img_id': img_ids, 'caption': caps}).set_index(['img_id'])
 
 enc_map = cPickle.load(open('dataset/enc_map.pkl', 'rb'))  # token => id
